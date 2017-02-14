6e70f06452fe8cb33952f46077547b6d4e6ba19f
Ben Falconer <ben@falconers.me.uk>
2017-02-14 14:24:33 +0000

    feat: Use a customizer file separate to the variables file

--- main.js
+++ main.js
@@ -1,87 +1,9 @@
-var sass;
-
+//path to the web worker
 var workerPath = 'node_modules/sass.js/dist/sass.worker.js';
-var base = '../../bootstrap-sass/assets/stylesheets/';
+//path to the base of where files are available from libsass
+var base = '../../../';
 
-var directory = '';
-
-var files = [
-	'_bootstrap.scss',
-	'_bootstrap-compass.scss',
-	'_bootstrap-mincer.scss',
-	'_bootstrap-sprockets.scss',
-	'bootstrap/_alerts.scss',
-	'bootstrap/_badges.scss',
-	'bootstrap/_breadcrumbs.scss',
-	'bootstrap/_button-groups.scss',
-	'bootstrap/_buttons.scss',
-	'bootstrap/_carousel.scss',
-	'bootstrap/_close.scss',
-	'bootstrap/_code.scss',
-	'bootstrap/_component-animations.scss',
-	'bootstrap/_dropdowns.scss',
-	'bootstrap/_forms.scss',
-	'bootstrap/_glyphicons.scss',
-	'bootstrap/_grid.scss',
-	'bootstrap/_input-groups.scss',
-	'bootstrap/_jumbotron.scss',
-	'bootstrap/_labels.scss',
-	'bootstrap/_list-group.scss',
-	'bootstrap/_media.scss',
-	'bootstrap/_mixins.scss',
-	'bootstrap/_modals.scss',
-	'bootstrap/_navbar.scss',
-	'bootstrap/_navs.scss',
-	'bootstrap/_normalize.scss',
-	'bootstrap/_pager.scss',
-	'bootstrap/_pagination.scss',
-	'bootstrap/_panels.scss',
-	'bootstrap/_popovers.scss',
-	'bootstrap/_print.scss',
-	'bootstrap/_progress-bars.scss',
-	'bootstrap/_responsive-embed.scss',
-	'bootstrap/_responsive-utilities.scss',
-	'bootstrap/_scaffolding.scss',
-	'bootstrap/_tables.scss',
-	'bootstrap/_theme.scss',
-	'bootstrap/_thumbnails.scss',
-	'bootstrap/_tooltip.scss',
-	'bootstrap/_type.scss',
-	'bootstrap/_utilities.scss',
-	'bootstrap/_variables.scss',
-	'bootstrap/_wells.scss',
-	'bootstrap/mixins/_alerts.scss',
-	'bootstrap/mixins/_background-variant.scss',
-	'bootstrap/mixins/_border-radius.scss',
-	'bootstrap/mixins/_buttons.scss',
-	'bootstrap/mixins/_center-block.scss',
-	'bootstrap/mixins/_clearfix.scss',
-	'bootstrap/mixins/_forms.scss',
-	'bootstrap/mixins/_gradients.scss',
-	'bootstrap/mixins/_grid-framework.scss',
-	'bootstrap/mixins/_grid.scss',
-	'bootstrap/mixins/_hide-text.scss',
-	'bootstrap/mixins/_image.scss',
-	'bootstrap/mixins/_labels.scss',
-	'bootstrap/mixins/_list-group.scss',
-	'bootstrap/mixins/_nav-divider.scss',
-	'bootstrap/mixins/_nav-vertical-align.scss',
-	'bootstrap/mixins/_opacity.scss',
-	'bootstrap/mixins/_pagination.scss',
-	'bootstrap/mixins/_panels.scss',
-	'bootstrap/mixins/_progress-bar.scss',
-	'bootstrap/mixins/_reset-filter.scss',
-	'bootstrap/mixins/_reset-text.scss',
-	'bootstrap/mixins/_resize.scss',
-	'bootstrap/mixins/_responsive-visibility.scss',
-	'bootstrap/mixins/_size.scss',
-	'bootstrap/mixins/_tab-focus.scss',
-	'bootstrap/mixins/_table-row.scss',
-	'bootstrap/mixins/_text-emphasis.scss',
-	'bootstrap/mixins/_text-overflow.scss',
-	'bootstrap/mixins/_vendor-prefixes.scss',
-];
-
+var sass;
 var editor;
 var variables = {};
 var currentUpdates = 0;
@@ -106,13 +28,8 @@ $(document).ready(function() {
 			comments: false,
 		},
 		function() {
-			sass.preloadFiles(base, directory, files, function callback() {
-				sass.readFile('bootstrap/_variables.scss', function(content) {
-					editor.getSession().setValue(content);
-					update();
-					parseCode();
-				});
-			});
+			var callback = setupWithContent;
+			loadFiles(callback);
 		}
 	);
 
@@ -121,21 +38,51 @@ $(document).ready(function() {
 	$('#easy_mode_button').on('click', showEasyMode);
 });
 
+function setupWithContent(customizerContent) {
+	parseCode(customizerContent);
+	editor.getSession().setValue(customizerContent);
+	update();
+}
+
+function loadFiles(callback) {
+	var fullFileList = otherFiles;
+	fullFileList.push(baseFile);
+	fullFileList.push(customizerFile);
+	fullFileList.push(variablesFile);
+
+	sass.preloadFiles(base, '', fullFileList, function() {
+		sass.readFile(customizerFile, function(customizerContent) {
+			if(customizerContent != '') {
+				callback(customizerContent);
+			}
+			else {
+				sass.readFile(variablesFile, function(variablesContent) {
+					sass.writeFile(customizerFile, variablesContent, function(success) {
+						if(!success)
+							throw new Error('could not write ' + customizerFile);
+						callback(variablesContent);
+					});
+				});
+			}
+		});
+	});
+}
+
 function update() {
 	var code = editor.getSession().getValue();
 	if(currentUpdates === 0)
 		$('#update_indicator').addClass('fa-spin');
 	currentUpdates++;
-	sass.writeFile('bootstrap/_variables.scss', code, function(success) {
+	sass.writeFile(customizerFile, code, function(success) {
 		if(!success) {
-			console.error('failed to write bootstrap/_variables.scss');
+			console.error('failed to write ' + customizerFile);
 			currentUpdates--;
 			if(currentUpdates === 0)
 				$('#update_indicator').removeClass('fa-spin');
 			return;
 		}
 
-		sass.compileFile('_bootstrap.scss', function(result) {
+		sass.compileFile(baseFile, function(result) {
 			if(result.status === 0) {
 				window.frames['preview'].contentDocument.getElementById('compiled_sass').innerHTML = result.text;
 				postUpdate();
@@ -165,13 +112,11 @@ function showEasyMode() {
 
 	$('#easy_mode_container').show();
 	$('#easy_mode_button').attr('disabled', true);
-	parseCode();
+	parseCode(editor.getSession().getValue());
 	postUpdate();
 }
 
-function parseCode() {
-	var code = editor.getSession().getValue();
-
+function parseCode(code) {
 	var state = {
 		ignoring: false,
 		variableComment: null,
@@ -206,6 +151,7 @@ function parseCode() {
 				spacing:   match[3],
 				value:     match[4],
 				lineEnd:   match[5],
+				modified:  false,
 			};
 
 			variables[match[2]] = variable;
@@ -280,6 +226,7 @@ function updateInput() {
 
 function _modifyVariable(value, variable) {
 	variable.value = value;
+	variable.modified = true;
 
 	var codeLines = editor.getSession().getValue().split('\n');
 	codeLines[variable.lineNo] = variable.lineStart + variable.value + variable.lineEnd;
@@ -291,7 +238,10 @@ function _modifyVariable(value, variable) {
 
 function compileVariables(variableList, callback) {
 	results = {};
-	var variableTest = '@import "bootstrap/variables";\n';
+	var variableTest = '';
+	variableTest += '@import "custom";\n';
+	variableTest += '@import "' + customizerFile + '";\n';
+	variableTest += '@import "' + variablesFile + '";\n';
 	_.each(variableList, function(variable) {
 		variableTest += 'test-properties { test-' + variable.name + ': ' + variable.value + ';}\n';
 	});
